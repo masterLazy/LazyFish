82de7dc996b84dfe0cce5b121c04b9f6298edb83
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-04-16 20:12:47 +0800

    Add files via upload

---
+++ basic_fish.hpp
@@ -0,0 +1,300 @@
+#pragma once
+/*****************************************************************************
+* basic_fish.hpp
+*****************************************************************************/
+
+namespace gobang
+{
+	class BasicFish :Fish
+	{
+	private:
+		int try_find(Board bd, int x, int y, int col, std::string str)
+		{
+			int h0 = bd[x][y];
+			int cnt0 = bd.find(str, col);
+			bd[x][y] = col;
+			int cnt1 = bd.find(str, col);
+			bd[x][y] = h0;
+			return cnt1 - cnt0;
+		}
+		//检测某个下标是否在范围内
+		bool safe(int x, int y)
+		{
+			return x >= 0 && x < 15 && y >= 0 && y < 15;
+		}
+	public:
+		std::array<int, 2> play(Board bd, int self, bool fbd = true)
+		{
+			//用于记录在每个点下棋的合适度
+			int m[15][15];
+			//初始化
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					m[x][y] = 0;
+				}
+			}
+			int enemy = Invert(self);
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					//攻击
+					if (bd.is_able(x, y, self, fbd))
+					{
+						//我方在这里下子后连五: 8
+						if (try_find(bd, x, y, self, "ooooo") > 0)
+						{
+							m[x][y] = max(m[x][y], 8);
+						}
+
+						//我方在这里下子后连四: 6
+						if (try_find(bd, x, y, self, "oooo") > 0)
+						{
+							m[x][y] = max(m[x][y], 6);
+						}
+
+						//我方在这里下子后连三: 4
+						//我方在这里下子后连多三: 6
+						int cnt = try_find(bd, x, y, self, "ooo");
+						if (cnt > 1)
+						{
+							m[x][y] = max(m[x][y], 6);
+						}
+						else if (cnt == 1)
+						{
+							m[x][y] = max(m[x][y], 4);
+						}
+
+						//我方在这里下子后连二: 2
+						if (try_find(bd, x, y, self, "oo") > 1)
+						{
+							m[x][y] = max(m[x][y], 2);
+						}
+					}
+					//防守
+					if (bd[x][y] == enemy)
+					{
+						//活一(可变为二): 1
+						if (safe(x - 1, y - 1) && bd[x - 1][y - 1] == 0 && m[x - 1][y - 1] < 1)
+						{
+							m[x - 1][y - 1] = 1;
+						}
+						if (safe(x, y - 1) && bd[x][y - 1] == 0 && m[x][y - 1] < 1)
+						{
+							m[x][y - 1] = 1;
+						}
+						if (safe(x + 1, y - 1) && bd[x + 1][y - 1] == 0 && m[x + 1][y - 1] < 1)
+						{
+							m[x + 1][y - 1] = 1;
+						}
+						if (safe(x + 1, y) && bd[x + 1][y] == 0 && m[x + 1][y] < 1)
+						{
+							m[x + 1][y] = 1;
+						}
+						if (safe(x + 1, y + 1) && bd[x + 1][y + 1] == 0 && m[x + 1][y + 1] < 1)
+						{
+							m[x + 1][y + 1] = 1;
+						}
+						if (safe(x, y + 1) && bd[x][y + 1] == 0 && m[x][y + 1] < 1)
+						{
+							m[x][y + 1] = 1;
+						}
+						if (safe(x - 1, y + 1) && bd[x - 1][y + 1] == 0 && m[x - 1][y + 1] < 1)
+						{
+							m[x - 1][y + 1] = 1;
+						}
+						if (safe(x - 1, y) && bd[x - 1][y] == 0 && m[x - 1][y] < 1)
+						{
+							m[x - 1][y] = 1;
+						}
+
+						//活二(可变为三): 3
+						//活二(可变为多三): 5
+						//"一"
+						if (safe(x - 1, y) && bd[x - 1][y] == 0 &&
+							bd[x][y] == enemy &&
+							safe(x + 1, y) && bd[x + 1][y] == enemy &&
+							safe(x + 2, y) && bd[x + 2][y] == 0)
+						{
+							if (safe(x - 2, y) && bd[x - 2][y] == 0)
+							{
+								if (m[x - 1][y] < 3)m[x - 1][y] = 3;
+								else if (m[x][y - 1] == 3)m[x - 1][y] = 5;
+							}
+							if (safe(x + 3, y) && bd[x + 3][y] == 0 && m[x + 2][y] < 3)
+							{
+								if (m[x + 2][y] < 3)m[x + 2][y] = 3;
+								else if (m[x][y + 2] == 3)m[x + 2][y] = 5;
+							}
+						}
+						//"|"
+						if (safe(x, y - 1) && bd[x][y - 1] == 0 &&
+							bd[x][y] == enemy &&
+							safe(x, y + 1) && bd[x][y + 1] == enemy &&
+							safe(x, y + 2) && bd[x][y + 2] == 0)
+						{
+							if (safe(x, y - 2) && bd[x][y - 2] == 0)
+							{
+								if (m[x][y - 1] < 3)m[x][y - 1] = 3;
+								else if (m[x][y - 1] == 3)m[x][y - 1] = 5;
+							}
+							if (safe(x, y + 3) && bd[x][y + 3] == 0)
+							{
+								if (m[x][y + 2] < 3)m[x][y + 2] = 3;
+								else if (m[x][y + 2] == 3)m[x][y + 2] = 5;
+							}
+						}
+						//"/"
+						if (safe(x - 1, y + 1) && bd[x - 1][y + 1] == 0 &&
+							bd[x][y] == enemy &&
+							safe(x + 1, y - 1) && bd[x + 1][y - 1] == enemy &&
+							safe(x + 2, y - 2) && bd[x + 2][y - 2] == 0)
+						{
+							if (safe(x - 2, y + 2) && bd[x - 2][y + 2] == 0 && m[x - 1][y + 1] < 3)
+							{
+								if (m[x - 1][y + 1] < 3)m[x - 1][y + 1] = 3;
+								else if (m[x - 1][y + 1] == 3)m[x - 1][y + 1] = 5;
+							}
+							if (safe(x + 3, y - 3) && bd[x + 3][y - 3] == 0 && m[x + 2][y - 2] < 3)
+							{
+								if (m[x + 2][y - 2] < 3)m[x + 2][y - 2] = 3;
+								else if (m[x + 2][y - 2] == 3)m[x + 2][y - 2] = 5;
+							}
+						}
+						//"\"
+						if (safe(x - 1, y - 1) && bd[x - 1][y - 1] == 0 &&
+							bd[x][y] == enemy &&
+							safe(x + 1, y + 1) && bd[x + 1][y + 1] == enemy &&
+							safe(x + 2, y + 2) && bd[x + 2][y + 2] == 0)
+						{
+							if (safe(x - 2, y - 2) && bd[x - 2][y + 2] == 0 && m[x - 1][y - 1] < 3)
+							{
+								if (m[x - 1][y - 1] < 3)m[x - 1][y - 1] = 3;
+								else if (m[x - 1][y - 1] == 3)m[x - 1][y - 1] = 5;
+							}
+							if (safe(x + 3, y + 3) && bd[x + 3][y - 3] == 0 && m[x + 2][y + 2] < 3)
+							{
+								if (m[x + 2][y + 2] < 3)m[x + 2][y + 2] = 3;
+								else if (m[x + 2][y + 2] == 3)m[x + 2][y + 2] = 5;
+							}
+						}
+						//活三(可变为四): 5
+						//活三(可变为多四): 6
+						//"一"
+						if (safe(x - 2, y) && bd[x - 2][y] == 0 &&
+							safe(x - 1, y) && bd[x - 1][y] == enemy &&
+							bd[x][y] == enemy &&
+							safe(x + 1, y) && bd[x + 1][y] == enemy &&
+							safe(x + 2, y) && bd[x + 2][y] == 0)
+						{
+							if (m[x - 2][y] < 5)m[x - 2][y] = 5;
+							else if (m[x - 2][y] == 5)m[x - 2][y] = 6;
+							if (m[x + 2][y] < 5)m[x + 2][y] = 5;
+							else if (m[x + 2][y] == 5)m[x + 2][y] = 6;
+						}
+						//"|"
+						if (safe(x, y - 2) && bd[x][y - 2] == 0 &&
+							safe(x, y - 1) && bd[x][y - 1] == enemy &&
+							bd[x][y] == enemy &&
+							safe(x, y + 1) && bd[x][y + 1] == enemy &&
+							safe(x, y + 2) && bd[x][y + 2] == 0)
+						{
+							if (m[x][y - 2] < 5)m[x][y - 2] = 5;
+							else if (m[x][y - 2] == 5)m[x][y - 2] = 6;
+							if (m[x][y + 2] < 5)m[x][y + 2] = 5;
+							else if (m[x][y + 2] == 5)m[x][y + 2] = 6;
+						}
+						//"/"
+						if (safe(x - 2, y + 2) && bd[x - 2][y + 2] == 0 &&
+							safe(x - 1, y + 1) && bd[x - 1][y + 1] == enemy &&
+							bd[x][y] == enemy &&
+							safe(x + 1, y - 1) && bd[x + 1][y - 1] == enemy &&
+							safe(x + 2, y - 2) && bd[x + 2][y - 2] == 0)
+						{
+							if (m[x - 2][y + 2] < 5)m[x - 2][y + 2] = 5;
+							else if (m[x - 2][y + 2] == 5)m[x - 2][y + 2] = 6;
+							if (m[x + 2][y - 2] < 5)m[x + 2][y - 2] = 5;
+							else if (m[x + 2][y - 2] == 5)m[x + 2][y - 2] = 6;
+						}
+						//"\"
+						if (safe(x - 2, y - 2) && bd[x - 2][y - 2] == 0 &&
+							safe(x - 1, y - 1) && bd[x - 1][y - 1] == enemy &&
+							bd[x][y] == enemy &&
+							safe(x + 1, y + 1) && bd[x + 1][y + 1] == enemy &&
+							safe(x + 2, y + 2) && bd[x + 2][y + 2] == 0)
+						{
+							if (m[x - 2][y - 2] < 5)m[x - 2][y - 2] = 5;
+							else if (m[x - 2][y - 2] == 5)m[x - 2][y - 2] = 6;
+							if (m[x + 2][y + 2] < 5)m[x + 2][y + 2] = 5;
+							else if (m[x + 2][y + 2] == 5)m[x + 2][y + 2] = 6;
+						}
+					}
+					if (bd[x][y] == 0)
+					{
+						//敌方在这里下棋后连五: 7
+						bd[x][y] = enemy;
+						if (bd.find("xxxxx", self) > 0 && m[x][y] < 7)m[x][y] = 7;
+						bd[x][y] = 0;
+						//敌方在这里下棋后形成"米"形中空型多活三: 5
+						int count = 0;//记录活三的个数
+						if (safe(x - 1, y) && bd[x - 1][y] == enemy &&
+							safe(x + 1, y) && bd[x + 1][y] == enemy &&
+							safe(x - 2, y) && bd[x - 2][y] == 0 &&
+							safe(x + 2, y) && bd[x + 2][y] == 0)
+						{
+							count++;
+						}
+						if (safe(x, y - 1) && bd[x][y - 1] == enemy &&
+							safe(x, y + 1) && bd[x][y + 1] == enemy &&
+							safe(x, y - 2) && bd[x][y - 2] == 0 &&
+							safe(x, y + 2) && bd[x][y + 2] == 0)
+						{
+							count++;
+						}
+						if (safe(x - 1, y - 1) && bd[x - 1][y - 1] == enemy &&
+							safe(x + 1, y + 1) && bd[x + 1][y + 1] == enemy &&
+							safe(x - 2, y - 2) && bd[x - 2][y - 2] == 0 &&
+							safe(x + 2, y + 2) && bd[x + 2][y + 2] == 0)
+						{
+							count++;
+						}
+						if (safe(x - 1, y + 1) && bd[x - 1][y + 1] == enemy &&
+							safe(x + 1, y - 1) && bd[x + 1][y - 1] == enemy &&
+							safe(x - 2, y + 2) && bd[x - 2][y + 2] == 0 &&
+							safe(x + 2, y - 2) && bd[x + 2][y - 2] == 0)
+						{
+							count++;
+						}
+						if (count >= 2)m[x][y] = 5;
+					}
+				}
+			}
+
+			//在最合适的地方下棋
+			int _x = -1, _y = -1;
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					if (bd.is_able(x, y, self, fbd))
+					{
+						if (_x == -1 || m[x][y] > m[_x][_y])
+						{
+							_x = x;
+							_y = y;
+						}
+						else if (m[x][y] == m[_x][_y] && rand() % 15 * 15 == 0)
+						{
+							_x = x;
+							_y = y;
+						}
+					}
+				}
+			}
+
+			return { _x,_y };
+		}
+	};
+}
---
+++ gobang.hpp
@@ -0,0 +1,244 @@
+#pragma once
+/*****************************************************************************
+* gobang.hpp
+*****************************************************************************/
+
+#include <array>
+#include <vector>
+#include <thread>
+
+#define _SILENCE_AMP_DEPRECATION_WARNINGS
+//#include <amp.h>
+
+namespace gobang
+{
+	const int G_EMPTY = 0;
+	const int G_BLACK = 1;
+	const int G_WHITE = 2;
+
+	//反转颜色
+	int Invert(int p)
+	{
+		if (p == G_WHITE)return G_BLACK;
+		else if (p == G_BLACK)return G_WHITE;
+		return p;
+	}
+
+	//棋盘
+	class Board
+	{
+	private:
+		int find_fbd()
+		{
+			int res = 0, b = G_BLACK;
+			//活三
+			res += find("-ooo-", b);
+			//活四 + 冲四
+			res += find("oooo-", b) - find("-oooo-", b);
+			//嵌五
+			res += find("ooo-o-", b) + find("-ooo-o", b) - find("-ooo-o-", b);
+			res += find("oo-oo-", b) - find("-oo-oo-", b);
+			return res;
+		}
+
+		std::array<int, 15> null_array;
+	public:
+		std::array<std::array<int, 15>, 15> map;
+
+		bool empty()
+		{
+			bool res = true;
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					res &= map[x][y] == G_EMPTY;
+				}
+			}
+			return res;
+		}
+		void clear()
+		{
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					map[x][y] = G_EMPTY;
+				}
+			}
+		}
+
+		//在棋盘中寻找某一线形结构, 返回数目
+		int find(const std::vector<int>& str, bool recursion = false)
+		{
+			if (str.size() == 0)return 0;
+			//遍历
+			int res = 0;
+			bool ok_0, ok_1, ok_2, ok_3;
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					ok_0 = ok_1 = ok_2 = ok_3 = true;
+					//在此位置查找结构
+					for (int i = 0; i < str.size(); i++)
+					{
+						if (x + i >= 15) ok_0 = false;
+						else ok_0 &= map[x + i][y] == str[i];
+
+						if (y + i >= 15) ok_1 = false;
+						else ok_1 &= map[x][y + i] == str[i];
+
+						if (x + i >= 15 || y + i >= 15) ok_2 = false;
+						else ok_2 &= map[x + i][y + i] == str[i];
+
+						if (x + i >= 15 || y - i < 0) ok_3 = false;
+						else ok_3 &= map[x + i][y - i] == str[i];
+					}
+					if (ok_0)res++;
+					if (ok_1)res++;
+					if (ok_2)res++;
+					if (ok_3)res++;
+				}
+			}
+			//如果不对称，还要反着来一遍
+			if (!recursion)
+			{
+				bool symmetry = true;//是否对称
+				for (int i = 0; i < str.size() / 2; i++)
+				{
+					symmetry &= str[i] == str[str.size() - i - 1];
+				}
+				if (!symmetry)
+				{
+					std::vector<int> trs(str.size());
+					for (int i = 0; i < str.size(); i++)
+					{
+						trs[i] = str[str.size() - i - 1];
+					}
+					res += find(trs, true);
+				}
+			}
+			return res;
+		}
+		int find_fast(const std::vector<int>& str, bool recursion = false)
+		{
+			//using namespace Concurrency;
+			//if (str.size() == 0)return 0;
+			//准备
+			//std::vector<int> res(15 * 15);
+			//array_view<int, 2> res_gpu(15, 15, res);
+			//res_gpu.discard_data();
+			return 0;
+		}
+		//在棋盘中寻找某一线形结构, 返回数目
+		//str: o表示颜色为p的棋子，x表示反色的棋子或边界，-表示空位
+		int find(const std::string& str, int col)
+		{
+			std::vector<int> temp;
+			for (char c : str)
+			{
+				switch (c)
+				{
+				case 'o':
+				case 'O':
+					temp.push_back(col);
+					break;
+
+				case 'x':
+				case 'X':
+					temp.push_back(Invert(col));
+					break;
+
+				case '-':
+					temp.push_back(G_EMPTY);
+					break;
+				}
+			}
+			if (temp.size() > 15)temp.resize(15);
+			return find(temp);
+		}
+
+		//获取获胜者(若没有，返回0)
+		int get_winner()
+		{
+			if (find("ooooo", G_WHITE) > 0)return G_WHITE;
+			if (find("ooooo", G_BLACK) > 0)return G_BLACK;
+			return 0;
+		}
+		//是否下满了
+		bool is_full()
+		{
+			bool full = true;
+			for (auto i : map)
+			{
+				for (auto j : i)
+				{
+					full &= j != G_EMPTY;
+				}
+			}
+			return full;
+		}
+		//棋局是否结束(包括下满了)
+		bool is_end()
+		{
+			return get_winner() != G_EMPTY || is_full();
+		}
+
+		//此处是否能下子
+		//col: 将下子的颜色
+		//fbd: 是否禁手
+		bool is_able(int x, int y, int col, bool fbd = true)
+		{
+			if (x < 0 || y < 0 || x >= 15 || y >= 15)return false;
+			//如果已有棋子
+			if (map[x][y] != G_EMPTY)return false;
+
+			//禁手
+			if (col == G_BLACK && fbd == true)
+			{
+				int cnt0 = find_fbd();
+
+				//试下
+				map[x][y] = col;
+
+				//长连
+				if (find("oooooo", col) > 0)
+				{
+					//还原
+					map[x][y] = G_EMPTY;
+					return false;
+				}
+
+
+				int cnt1 = find_fbd();
+
+				if (cnt1 - cnt0 > 1)
+				{
+					map[x][y] = G_EMPTY;
+					return false;
+				}
+
+				//还原
+				map[x][y] = G_EMPTY;
+			}
+
+			return true;
+		}
+
+		std::array<int, 15>& operator[](unsigned i)
+		{
+			if (i < 15)return map[i];
+			else return null_array;
+		}
+	};
+
+
+	class Fish
+	{
+	public:
+		virtual std::array<int, 2> play(Board bd, int self, bool forbidden = true) = 0;
+	};
+}
+
+#include "basic_fish.hpp"
