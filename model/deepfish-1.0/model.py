a9797514ce11e016e08a10072e06da6ff7d06247
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-04-16 23:09:54 +0800

    Add files via upload

--- model/deepfish-1.0/model.py
+++ model/deepfish-1.0/model.py
@@ -1,4 +1,4 @@
-﻿# U-Net Model
+﻿# DeepFish Model
 
 import torch
 from torch import nn
@@ -9,38 +9,42 @@ class Model(nn.Module):
         super(Model, self).__init__()
 
         self.seq = nn.Sequential(
+            # Down
             nn.Conv2d(1, 8, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((8,13,13)),
-            
+            nn.LayerNorm((8, 13, 13)),
+            #
             nn.Conv2d(8, 16, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((16,11,11)),
-            
+            nn.LayerNorm((16, 11, 11)),
+            #
             nn.Conv2d(16, 32, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((32,9,9)),
-            
+            nn.LayerNorm((32, 9, 9)),
+            #
             nn.Conv2d(32, 64, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((64,7,7)),
-            
+            nn.LayerNorm((64, 7, 7)),
+            # Up
             nn.ConvTranspose2d(64, 32, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((32,9,9)),
-            
+            nn.LayerNorm((32, 9, 9)),
+            #
             nn.ConvTranspose2d(32, 16, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((16,11,11)),
-            
+            nn.LayerNorm((16, 11, 11)),
+            #
             nn.ConvTranspose2d(16, 8, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((8,13,13)),
-            
+            nn.LayerNorm((8, 13, 13)),
+            #
             nn.ConvTranspose2d(8, 1, 3),
             nn.LeakyReLU(),
-            nn.LayerNorm((1,15,15))
         )
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        return self.seq(x)
+        x = self.seq(x)
+        b, c, h, w = x.size()
+        x = self.softmax(x.view(b, -1)).view(b, c, h, w)
+        return x
